93260879c60d07d601a63f030dbf5ffc6901e2a9
Matusalém <matusalemdedeus@gmail.com>
2024-12-02 14:52:18 -0300

    migração do schema

---
+++ drizzle/0002_dashing_matthew_murdock.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text(20) NOT NULL,
+	`email` text(60) NOT NULL,
+	`password` text NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `unique_email` ON `users` (`email`);--> statement-breakpoint
+ALTER TABLE `categories` ADD `userId` integer REFERENCES users(id);--> statement-breakpoint
+CREATE INDEX `user_id_idx` ON `categories` (`userId`);--> statement-breakpoint
+CREATE INDEX `stock_id_idx` ON `categories` (`stockId`);--> statement-breakpoint
+ALTER TABLE `products` ADD `userId` integer REFERENCES users(id);--> statement-breakpoint
+CREATE INDEX `user_id_idx` ON `products` (`userId`);--> statement-breakpoint
+CREATE INDEX `stock_id_idx` ON `products` (`stockId`);--> statement-breakpoint
+CREATE INDEX `category_id_idx` ON `products` (`categoryId`);--> statement-breakpoint
+ALTER TABLE `stocks` ADD `userId` integer REFERENCES users(id);--> statement-breakpoint
+CREATE INDEX `user_id_idx` ON `stocks` (`userId`);
