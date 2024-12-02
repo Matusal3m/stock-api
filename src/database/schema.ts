d97d07313a5a1e6521f7f6e5166d876199dd8e34
Matusalém <matusalemdedeus@gmail.com>
2024-12-02 14:51:05 -0300

    criação da tabela users e relacionamento desta com as outra tabelas

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -6,26 +6,63 @@ import {
   index,
 } from "drizzle-orm/sqlite-core";
 
-export const stocks = sqliteTable("stocks", {
-  id: int().primaryKey({ autoIncrement: true }),
-  name: text("name", { length: 20 }).notNull(),
-});
+export const users = sqliteTable(
+  "users",
+  {
+    id: int().primaryKey({ autoIncrement: true }),
+    name: text("name", { length: 20 }).notNull(),
+    email: text("email", { length: 60 }).notNull(),
+    password: text("password").notNull(),
+  },
+  (table) => ({
+    emailIndex: uniqueIndex("unique_email").on(table.email),
+  })
+);
 
-export const categories = sqliteTable("categories", {
-  id: int().primaryKey({ autoIncrement: true }),
-  name: text("name", { length: 20 }).notNull(),
-  stockId: int().references(() => stocks.id),
-});
+export const stocks = sqliteTable(
+  "stocks",
+  {
+    id: int().primaryKey({ autoIncrement: true }),
+    name: text("name", { length: 20 }).notNull(),
+    userId: int().references(() => users.id),
+  },
+  (table) => ({
+    userIdIdx: index("user_id_idx").on(table.userId),
+  })
+);
 
-export const products = sqliteTable("products", {
-  id: int().primaryKey({ autoIncrement: true }),
-  name: text("name", { length: 20 }).notNull(),
-  description: text("description", { length: 60 }),
-  quantity: int("quantity", { mode: "number" }).notNull(),
-  categoryId: int("categoryId")
-    .references(() => categories.id)
-    .notNull(),
-  stockId: int()
-    .references(() => stocks.id)
-    .notNull(),
-});
+export const categories = sqliteTable(
+  "categories",
+  {
+    id: int().primaryKey({ autoIncrement: true }),
+    name: text("name", { length: 20 }).notNull(),
+    stockId: int().references(() => stocks.id),
+    userId: int().references(() => users.id),
+  },
+  (table) => ({
+    userIdIdx: index("user_id_idx").on(table.userId),
+    stockIdIdx: index("stock_id_idx").on(table.stockId),
+  })
+);
+
+export const products = sqliteTable(
+  "products",
+  {
+    id: int().primaryKey({ autoIncrement: true }),
+    name: text("name", { length: 20 }).notNull(),
+    description: text("description", { length: 60 }),
+    quantity: int("quantity", { mode: "number" }).notNull(),
+    categoryId: int("categoryId")
+      .references(() => categories.id)
+      .notNull(),
+    stockId: int()
+      .references(() => stocks.id)
+      .notNull(),
+    userId: int().references(() => users.id),
+  },
+  (table) => ({
+    userIdIdx: index("user_id_idx").on(table.userId),
+    stockIdIdx: index("stock_id_idx").on(table.stockId),
+    categoryIdIdx: index("category_id_idx").on(table.categoryId),
+  })
+);
