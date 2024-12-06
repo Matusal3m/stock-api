1e56a8d5020ce93667c32fce4fd09055977536e4
Matusalém <matusalemdedeus@gmail.com>
2024-12-06 04:20:12 -0300

    adiciona cors ao server

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import express from "express";
 import configRoutes from "./routes";
 import configGlobalMiddlewares from "./middlewares";
 import "dotenv/config"
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(express.json());
+app.use(cors())
 
 configGlobalMiddlewares(app);
 configRoutes(app);
