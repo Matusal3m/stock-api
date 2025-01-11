396fef7da601d9c4e30da8674378ed8cc13176e4
Matusal3m <matusalem.dev@gmail.com>
2025-01-11 01:50:39 -0300

    removed dashboard route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import ProductsRoutes from "./ProductsRoutes";
 import CategoriesRoutes from "./CategoriesRoutes";
-import DashboardRoutes from "./DashboardRoutes";
 import StocksRoutes from "./StocksRoutes";
 import AuthenticationRoutes from "./AuthenticationRoutes";
 import StatusRoutes from "./StatusRoutes";
@@ -13,7 +12,6 @@ export default (app: Application) => {
   router.use("/products", AuthMiddleware, ProductsRoutes);
   router.use("/categories", AuthMiddleware, CategoriesRoutes);
   router.use("/stocks", AuthMiddleware, StocksRoutes);
-  router.use("/dashboard", AuthMiddleware, DashboardRoutes);
 
   router.use("/auth", AuthenticationRoutes);
 
